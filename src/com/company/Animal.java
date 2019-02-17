bc395c726debd2e74dde8e4ed0c5edd362a09529
Awet Fessha <awetkifle94@gmail.com>
2019-02-17 23:14:04 +0000

    animal class getters and setters

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,49 @@
 package com.company;
 
 public class Animal {
+    private String animalID;
+    private String animalName;
+    private String animalFamiliy;
+    private String animalSpecies;
+    private String cageAssignment;
+
+    public String getAnimalID() {
+        return animalID;
+    }
+
+    public void setAnimalID(String animalID) {
+        this.animalID = animalID;
+    }
+
+    public String getAnimalName() {
+        return animalName;
+    }
+
+    public void setAnimalName(String animalName) {
+        this.animalName = animalName;
+    }
+
+    public String getAnimalFamiliy() {
+        return animalFamiliy;
+    }
+
+    public void setAnimalFamiliy(String animalFamiliy) {
+        this.animalFamiliy = animalFamiliy;
+    }
+
+    public String getAnimalSpecies() {
+        return animalSpecies;
+    }
+
+    public void setAnimalSpecies(String animalSpecies) {
+        this.animalSpecies = animalSpecies;
+    }
+
+    public String getCageAssignment() {
+        return cageAssignment;
+    }
+
+    public void setCageAssignment(String cageAssignment) {
+        this.cageAssignment = cageAssignment;
+    }
 }
