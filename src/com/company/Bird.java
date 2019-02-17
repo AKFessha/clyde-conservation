3a571da1ef0cc37bfc87e9f1bf236640a728d5a8
Awet Fessha <awetkifle94@gmail.com>
2019-02-17 23:17:38 +0000

    add getters and setters for bird class

--- src/com/company/Bird.java
+++ src/com/company/Bird.java
@@ -1,4 +1,24 @@
 package com.company;
 
 public class Bird {
+    private String birdType;
+    private String nestType;
+
+    //Getters and setters
+
+    public String getBirdType() {
+        return birdType;
+    }
+
+    public void setBirdType(String birdType) {
+        this.birdType = birdType;
+    }
+
+    public String getNestType() {
+        return nestType;
+    }
+
+    public void setNestType(String nestType) {
+        this.nestType = nestType;
+    }
 }
