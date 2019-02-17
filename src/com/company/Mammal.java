4199d86769b575b285f1d24ba67e4cc06b748584
Awet Fessha <awetkifle94@gmail.com>
2019-02-17 23:21:08 +0000

    Getters and setters for mammal class

--- src/com/company/Mammal.java
+++ src/com/company/Mammal.java
@@ -1,4 +1,24 @@
 package com.company;
 
 public class Mammal {
+    private String habitat;
+    private String lifeSpan;
+
+    //Getters and setters
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public String getLifeSpan() {
+        return lifeSpan;
+    }
+
+    public void setLifeSpan(String lifeSpan) {
+        this.lifeSpan = lifeSpan;
+    }
 }
