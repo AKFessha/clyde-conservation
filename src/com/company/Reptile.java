226da0e76c91256e2c8093aa3d0d70694b8ab69c
Awet Fessha <awetkifle94@gmail.com>
2019-02-17 23:19:20 +0000

    Geters and setters for reptile class

--- src/com/company/Reptile.java
+++ src/com/company/Reptile.java
@@ -1,4 +1,24 @@
 package com.company;
 
 public class Reptile {
+    private String enviornment;
+    private String cageTemp;
+
+    //Getters and Setters
+
+    public String getEnviornment() {
+        return enviornment;
+    }
+
+    public void setEnviornment(String enviornment) {
+        this.enviornment = enviornment;
+    }
+
+    public String getCageTemp() {
+        return cageTemp;
+    }
+
+    public void setCageTemp(String cageTemp) {
+        this.cageTemp = cageTemp;
+    }
 }
