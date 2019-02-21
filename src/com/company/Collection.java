151164d6dfe0741c6c2fffc09eac466bae54704f
Awet Fessha <awetkifle94@gmail.com>
2019-02-21 00:41:43 +0000

    create constructor and initalise all arraylists

--- src/com/company/Collection.java
+++ src/com/company/Collection.java
@@ -1,4 +1,36 @@
 package com.company;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Collection {
+
+    /*
+     * VARIABLES
+     * ArrayList for all Enclosures/ Cages / Animals / Keepers declared
+     * FileInputOutput object declared
+     */
+    private ArrayList<Cage> allCages;
+    private ArrayList<Animal> allAnimals;
+    private ArrayList<Keeper> allKeepers;
+    private ArrayList<Enclosure> allEnclosures;
+    private FileInputOutput data;
+
+    /*
+     * CONSTRUCTOR
+     * Initialise all arrayLists
+     * Instantiate FileInputOutput object
+     * Call readData() method to populate data structures from
+     * text file data
+     * Collection object instantiated on program start so data is
+     * automatically read
+     */
+
+    public Collection() throws IOException {
+        allCages = new ArrayList<>();
+        allAnimals = new ArrayList<>();
+        allKeepers= new ArrayList<>();
+        allEnclosures= new ArrayList<>();
+        data = new FileInputOutput();
+    }
 }
