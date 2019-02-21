5b32a608c0deead0d68c3a2a96f38993e12cf626
Awet Fessha <awetkifle94@gmail.com>
2019-02-21 00:45:44 +0000

    read all data

--- src/com/company/Collection.java
+++ src/com/company/Collection.java
@@ -34,6 +34,31 @@ public class Collection {
         data = new FileInputOutput();
     }
 
+    /*
+     * Read Data method
+     * Passes each ArrayList to their respective
+     * read data method in FileInputOutput class
+     * ArrayLists in Cage class also defined here
+     * after main ArrayLists are populated
+     */
+    public void readData() throws IOException {
+        data.readAnimalData(allAnimals);
+        data.readKeeperData(allKeepers);
+        data.readCageData(allCages);
+        data.readEnclosureData(allEnclosures);
+        for(Cage cage : allCages) {
+            for(Animal animal : allAnimals) {
+                if(animal.getCageAssignment().equals(cage.getCageID())) {
+                    cage.getCagedAnimals().add(animal);
+                }
+            }
+            for(Keeper keeper : allKeepers) {
+                if(keeper.getCageAssignment().contains(cage.getCageID())) {
+                    cage.getCagedKeepers().add(keeper);
+                }
+            }
+        }
+    }
     //Getters and setters
 
     public ArrayList<Cage> getAllCages() {
