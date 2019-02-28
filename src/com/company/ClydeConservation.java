655b13ec95e6b745d7aca8650aff92b18bef3ce8
Awet Fessha <awetkifle94@gmail.com>
2019-02-28 16:10:16 +0000

    instantiate a menu object

--- src/com/company/ClydeConservation.java
+++ src/com/company/ClydeConservation.java
@@ -1,9 +1,20 @@
 package com.company;
 
+import java.io.IOException;
+
+/*
+ * Awet Fessha
+ * Clyde conservation program
+ * 28/02/19
+ * ClydeConservation Class
+ */
 public class ClydeConservation {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        //Instantiate menu object
+              Menu myMenu= new Menu();
+              myMenu.menuProcessor();
 
     }
 }
