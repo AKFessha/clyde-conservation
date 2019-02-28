58c3ebe46ea91203f45610167620f8349dfa26c1
Awet Fessha <awetkifle94@gmail.com>
2019-02-28 15:49:35 +0000

    display menu method

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -1,4 +1,19 @@
 package com.company;
 
 public class Menu {
+
+
+    public void displayMainMenu(){
+        System.out.println("1. List all available Enclosures");
+        System.out.println("2. List all available Cages with their Animals and Keepers");
+        System.out.println("3. Add a new Cage");
+        System.out.println("4. List all available Animals ");
+        System.out.println("5. Add new Animal and assign it to Cage");
+        System.out.println("6. Unassign Animal from Cage");
+        System.out.println("7. List all available Keepers");
+        System.out.println("8. Add a new Keeper");
+        System.out.println("9. Assign keeper to cage");
+        System.out.println("10. Unassign Keeper from Cage");
+        System.out.println("11. Save all the current details and exit");
+    }
 }
